ff9e868e56c3956dc38ce1f4e76dbe33dd2a7d16
Sinaver Idris <sinavercrimea@gmail.com>
2023-01-24 22:19:42 +0400

    added draft home page with short project info

--- app/src/routes/App.tsx
+++ app/src/routes/App.tsx
@@ -1,8 +1,6 @@
 import { useState, useMemo } from 'react';
 import { Link } from 'react-router-dom';
 
-import '../styles/App.css';
-
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import GlobalStyles from '@mui/material/GlobalStyles';
@@ -12,13 +10,11 @@ import { AppBar, Button, IconButton, Toolbar, Typography } from '@mui/material';
 import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
 import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
 
-import reactLogo from '../assets/react.svg';
 import HomeLogo from '../common/HomeLogo';
 import muiTheme from '../common/themeUtils';
 
 function App() {
-  const [darkMode, setDarkMode] = useState(true);
-  const [count, setCount] = useState(0);
+  const [darkMode, setDarkMode] = useState(false);
 
   const theme = useMemo(() => muiTheme(darkMode), [darkMode]);
 
@@ -39,32 +35,6 @@ function App() {
           <Button variant="contained">Connect Wallet</Button>
         </Toolbar>
       </AppBar>
-      <div className="App">
-        <div>
-          <a href="https://vitejs.dev" target="_blank">
-            <img src="/vite.svg" className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://vitejs.dev" target="_blank">
-            <img src="/cubes.svg" className="logo" alt="ReBuild3 logo" />
-          </a>
-          <a href="https://reactjs.org" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
-        <h1>Vite + React</h1>
-        <Link to={'/'}>Home</Link>
-        <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test HMR
-          </p>
-        </div>
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
-        </p>
-      </div>
     </ThemeProvider>
   );
 }
--- app/src/routes/Home.tsx
+++ app/src/routes/Home.tsx
@@ -5,7 +5,17 @@ import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import GlobalStyles from '@mui/material/GlobalStyles';
 
-import { AppBar, Button, IconButton, Toolbar, Typography } from '@mui/material';
+import {
+  AppBar,
+  Button,
+  Card,
+  Container,
+  IconButton,
+  Toolbar,
+  Typography,
+  Box,
+  Stack,
+} from '@mui/material';
 
 import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
 import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
@@ -13,8 +23,12 @@ import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
 import HomeLogo from '../common/HomeLogo';
 import muiTheme from '../common/themeUtils';
 
+import { ReactComponent as Love } from '../assets/loving.svg';
+import { ReactComponent as LoveInverted } from '../assets/loving_inverted.svg';
+import { blue, green, red } from '@mui/material/colors';
+
 export default function Home() {
-  const [darkMode, setDarkMode] = useState(true);
+  const [darkMode, setDarkMode] = useState(false);
 
   const theme = useMemo(() => muiTheme(darkMode), [darkMode]);
 
@@ -26,6 +40,7 @@ export default function Home() {
           body: { backgroundColor: darkMode ? '#242424' : '#f8fafc' },
         }}
       />
+
       <AppBar position="fixed">
         <Toolbar variant="dense">
           <HomeLogo flexGrow={1} />
@@ -37,6 +52,68 @@ export default function Home() {
           </Button>
         </Toolbar>
       </AppBar>
+
+      <Container>
+        <Card sx={{ my: 10, border: 2, borderRadius: 16 }} variant="elevation">
+          <Box
+            sx={{
+              m: 5,
+              display: 'flex',
+              flexDirection: 'row',
+              flexWrap: 'wrap',
+              alignItems: 'center',
+            }}>
+            <Stack sx={{ spacing: 2, flex: 1 }}>
+              <Typography color="grey" variant="h5">
+                DECENTRALIZED CROWDFUNDING SOLUTION
+              </Typography>
+              <Typography variant="h3">
+                ReBuild3 helps global community to bring back what was destroyed
+              </Typography>
+              <Typography m="10" color="grey" variant="h6">
+                enforced by smart contracts to bring transperancy into the
+                fundraising process, safeguard donations, and validate the
+                expenditure.
+              </Typography>
+            </Stack>
+            {darkMode ? <LoveInverted width={350} /> : <Love width={350} />}
+          </Box>
+        </Card>
+        <Box
+          sx={{
+            m: 5,
+            display: 'flex',
+            flexDirection: 'row',
+            //flexWrap: 'wrap',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+          }}>
+          <Box sx={{ m: 2, spacing: 2, flexWrap: 'wrap' }}>
+            <Typography color={red[400]} variant="h4">
+              Submit
+            </Typography>
+            <Typography color="grey" variant="body1">
+              Provide information on fundraising campaign.
+            </Typography>
+          </Box>
+          <Box sx={{ m: 2, spacing: 2, flexWrap: 'wrap' }}>
+            <Typography color={blue[400]} variant="h4">
+              Release
+            </Typography>
+            <Typography color="grey" variant="body1">
+              Validator confirms the details of campaign and releases the funds
+            </Typography>
+          </Box>
+          <Box sx={{ m: 2, spacing: 2, flexWrap: 'wrap' }}>
+            <Typography color={green[400]} variant="h4">
+              Validate
+            </Typography>
+            <Typography color="grey" variant="body1">
+              Validator check what was rebuilt.
+            </Typography>
+          </Box>
+        </Box>
+      </Container>
     </ThemeProvider>
   );
 }
